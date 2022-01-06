547bd563c243c644a93bf2ca463c4727db710c9a
Shivam <shivamssingh786@gmail.com>
2022-01-06 14:22:39 +0530

    UPDATE

--- Shivam_internship/src/dna_extractor/main.cpp
+++ Shivam_internship/src/dna_extractor/main.cpp
@@ -1,127 +1,170 @@
-#include <Arduino.h>
-#include <Servo.h>
-#define EXT_INTERRUPT 12
-#define LIM_SWITCH_PIN1 7
-#define LIM_SWITCH_PIN2 8
-#define MOTORPIN1 9
-#define MOTORPIN2 10
-#define SERVOPIN1 11
-#define SERVOPIN2 12
-#define HOME 0                       
-#define BAUDRATE 9600
-#define DELAY 10000
-Servo myservo[2];
-volatile bool INT_FLAG = LOW;
-void InterruptFunc();
-int chamber = 10;
+#include<REQLIBS.h>
 unsigned long FutureTime  = 0;
 unsigned long CurrentTime = 0;
+volatile bool flag = 0;
+void FUNCTION();
+void DOWN();
+void UP();
+void DRIVER();
+int CYCLE = 1;
+Servo myservo[2];
 
 void setup() 
 {
-pinMode(EXT_INTERRUPT,    INPUT);          // external interrupt pin
-pinMode(LIM_SWITCH_PIN1, INPUT);           // lower limit switch
-pinMode(LIM_SWITCH_PIN2, INPUT);           // upper limit switch
-pinMode(MOTORPIN1,      OUTPUT);
-pinMode(MOTORPIN2,      OUTPUT); 
-pinMode(SERVOPIN1,      OUTPUT);           // servo1
-pinMode(SERVOPIN2,      OUTPUT);           // servo2
-myservo[0].attach(SERVOPIN1); 
-myservo[1].attach(SERVOPIN2);
-myservo[0].write(HOME);                     // by default stay at home
-myservo[1].write(HOME);                     // by default stay at home
-digitalWrite(MOTORPIN1, LOW);               // initially stop
-digitalWrite(MOTORPIN2, LOW);               // initially stop
-attachInterrupt(digitalPinToInterrupt (EXT_INTERRUPT), InterruptFunc, RISING);
-Serial.begin(BAUDRATE);
+ pinMode(SWITCH1, INPUT);
+ pinMode(SWITCH2, INPUT); 
+ pinMode(INTERRUPT, INPUT);
+ pinMode(MOTORPIN1, OUTPUT);
+ pinMode(MOTORPIN2, OUTPUT); 
+ pinMode(PWMPIN, OUTPUT);
+ pinMode(SERVOPIN1, OUTPUT);
+ pinMode(SERVOPIN2, OUTPUT); 
+ myservo[0].attach(SERVOPIN1);       
+ myservo[1].attach(SERVOPIN2);            
+ digitalWrite(MOTORPIN1, LOW);               // initially stop
+ digitalWrite(MOTORPIN2, LOW); 
+ attachInterrupt(digitalPinToInterrupt (INTERRUPT), FUNCTION, RISING);
+ Serial.begin(BAUDRATE);
 }
 
 void loop() 
 {
-  if(INT_FLAG == 1)                   // if interrupt occurs make flag 1 and start system
+ if(flag==1)                                // if interrupt occurs externally
   {
-     Serial.println("Flag is 1");
-    for(chamber = 10 ; chamber <= 60 ; chamber = chamber + 10)
-    { 
-      while(digitalRead(LIM_SWITCH_PIN1)!= LOW)
-      {
-       Serial.println("Moving down"); 
-       digitalWrite(MOTORPIN1, HIGH);       // move down            
-       digitalWrite(MOTORPIN2, LOW);
-       if(INT_FLAG != 1)
-       {
-         Serial.println("INT OCCURED");
-         break;
-       }
-      }
-      digitalWrite(MOTORPIN1, LOW);
-      myservo[0].write(chamber);            // set servo to chamber 1       
-      myservo[1].write(chamber);
-      Serial.print("SERVO SET TO:");
-      Serial.println(chamber);
-      while(digitalRead(LIM_SWITCH_PIN2)!= LOW)
-      {
-       Serial.println("Moving Up");  
-       digitalWrite(MOTORPIN1, LOW);       // move up            
-       digitalWrite(MOTORPIN2, HIGH);   
-       if(INT_FLAG != 1)
-       {
-         Serial.println("INT OCCURED");
-         break;
-       }                                                  
-      }
-      digitalWrite(MOTORPIN2, LOW);
-      CurrentTime = millis();
-      FutureTime = CurrentTime + DELAY;
-      while(CurrentTime <= FutureTime)
-      {
-        Serial.println("EXTRACTING DNA");
-        if(INT_FLAG != 1)
-       {
-         Serial.println("INT OCCURED");
-         break;
-       }
-      }
-      if(chamber == 60)
-      {
-       while(digitalRead(LIM_SWITCH_PIN1)!= LOW)
-       {
-        Serial.println("Moving down"); 
-        digitalWrite(MOTORPIN1, HIGH);       // move down            
-        digitalWrite(MOTORPIN2, LOW);
-        if(INT_FLAG != 1)
-         {
-          Serial.println("INT OCCURED");
-          break;
-         }
-       } 
-       myservo[0].write(HOME);                    // move servo at home position
-       myservo[1].write(HOME); 
-      }
-    }
-    Serial.println("DNA EXTRACTION COMPLETE");
-  }
-  else                                         // if no interrupt   
-  { 
-   Serial.println("No Interrupt Occured");                                          
-   myservo[0].write(HOME);                    // move servo at home position
-   myservo[1].write(HOME);                     
-    if(digitalRead(LIM_SWITCH_PIN1) == HIGH)      // if limit switch is open 
+    if(CYCLE <=6)                                // count CYCLES
     {
-      Serial.println("MOVING DOWN");
-      digitalWrite(MOTORPIN1, HIGH);                // drive motor downwards
-      digitalWrite(MOTORPIN2, LOW);
-    }
-    else                                           // if limit switch closed
-    {  
-      digitalWrite(MOTORPIN1, LOW);               // stop the motor
-      digitalWrite(MOTORPIN2, LOW);
-      Serial.println("IDLE MODE");
-    } 
+       DOWN();                                  // move DOWN
+      
+       DRIVER();                                // set servos to angle according to data
+       Serial.print("SERVO SET TO:");           // serial print status of cycle
+       Serial.println(CYCLE);
+       UP();                                    // move DOWN
+      
+       CurrentTime = millis();                   //   delay 
+       FutureTime = CurrentTime + DELAY;         
+       while(millis() <= FutureTime)
+       {
+         Serial.println("EXTRACTING DNA");
+         if(flag!= 1)
+        {
+          Serial.println("INTERRUPT OCCURED");
+          break;
+        }
+       } 
+       if(CYCLE == 6)                                    // if last cycle
+       {   
+          DOWN();                                          // then go DOWN
+        
+          myservo[0].write(HOME);                          //set servos to home
+          myservo[1].write(HOME);                          
+          Serial.println("DNA EXTRACTION COMPLETE");       // print completion msg
+       }
+    CYCLE++;                                             // increment the cycle
+    }   
   }
+ else                                                   // if interrupt not occured
+  {
+     Serial.println("INTERRUPT NOT OCCURED");             // print status
+     if(digitalRead(SWITCH1) == LOW)                     // if motor is somewhere in mid move DOWN
+    
+     {                                           
+      Serial.println("MOVING DOWN");                    // print status of moving DOWN
+    
+      digitalWrite(MOTORPIN1, LOW);
+      digitalWrite(MOTORPIN2, HIGH);
+      digitalWrite(PWMPIN, HIGH); 
+     }
+     else                                              // if motor reaches DOWN
+     
+     {
+      digitalWrite(PWMPIN,0);                            // then stop
+      myservo[0].write(HOME);                           // set servos at home pos
+      myservo[1].write(HOME);  
+      Serial.println("IDLE STATE");                   // print status as idle
+     }
+  }
+  
 }
 
-void InterruptFunc()
+void FUNCTION()                                      // if interrupt occurs
 {
-  INT_FLAG = !INT_FLAG;                        // toggle interrupt flag
+  flag = !flag;                                     // toggle flag state
+}
+
+void DRIVER()                                      // set servos to diff positions based on cycles
+{
+  
+  switch(CYCLE)
+  {
+    case 1:
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
+
+    case 2:
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
+
+    case 3:
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
+
+    case 4:
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
+
+    case 5:
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
+
+    case 6:
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
+
+    default:
+    myservo[0].write(HOME);
+    myservo[1].write(HOME);
+  }
+  
+}
+
+void DOWN()                                             // DOWN logic
+{
+      while(digitalRead(SWITCH1) == LOW)               // while lower limit switch is low move DOWN
+    
+      {
+       Serial.println("MOVING DOWN");
+       digitalWrite(MOTORPIN1, LOW);
+       digitalWrite(MOTORPIN2, HIGH);
+       digitalWrite(PWMPIN,HIGH); 
+       if(flag!= 1)                                   // amid this, if interrupt occurs 
+        {
+        Serial.print("INTERRUPT OCCURED");             // then print and break 
+        break; 
+        }  
+      }     
+      digitalWrite(PWMPIN,0);                          // stop the motor
+}
+
+void UP()
+{
+    while(digitalRead(SWITCH2) == LOW)
+      {
+      Serial.println("MOVING UP");
+      digitalWrite(MOTORPIN1, HIGH);
+      digitalWrite(MOTORPIN2, LOW);
+      digitalWrite(PWMPIN,HIGH);
+
+       if(flag!= 1)
+       {
+        Serial.print("INTERRUPT OCCURED");
+        break; 
+       }  
+      }
+      digitalWrite(PWMPIN,0);
 }
