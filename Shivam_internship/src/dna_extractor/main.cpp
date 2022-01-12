8562558408100628649383015d214169af49ea24
Shivam Singh <95620523+shivamssingh786@users.noreply.github.com>
2022-01-12 20:04:23 +0530

    Update main.cpp

--- Shivam_internship/src/dna_extractor/main.cpp
+++ Shivam_internship/src/dna_extractor/main.cpp
@@ -1,288 +1,170 @@
-#include <mcp_can.h>
-#include <SPI.h>
-#include<Servo.h>
-#define PWMPIN    3
-#define SWITCH1   4             //lower limit switch
-#define SWITCH2   5             //upper limit switch 
-#define MOTORPIN1 6
-#define MOTORPIN2 7
-#define SERVOPIN1 9
-#define SERVOPIN2 10
-#define BAUDRATE 115200
-#define DELAY 5000
-#define HOME 0
-#define SPEED 200
+#include<REQLIBS.h>
 unsigned long FutureTime  = 0;
 unsigned long CurrentTime = 0;
+volatile bool flag = 0;
+void FUNCTION();
 void DOWN();
 void UP();
 void DRIVER();
 int CYCLE = 1;
-bool idle_flag = 0;
-bool bit_check = 0;
-bool process = 0;
-bool single1 = 0;
-bool single2 = 0;
-const int SPI_CS_PIN = 8;
-MCP_CAN CAN(SPI_CS_PIN);
 Servo myservo[2];
-enum process
-{
-  IDLE = 0,
-  START,
-  RUNNING,
-  STOP,
-  COMPLETE
-};
-uint8_t estate = IDLE;
 
-void setup()
+void setup() 
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(SWITCH1, INPUT_PULLUP);
-  pinMode(SWITCH2, INPUT_PULLUP);
-  pinMode(MOTORPIN1, OUTPUT);
-  pinMode(MOTORPIN2, OUTPUT);
-  pinMode(PWMPIN, OUTPUT);
-  pinMode(SERVOPIN1, OUTPUT);
-  pinMode(SERVOPIN2, OUTPUT);
-  myservo[0].attach(SERVOPIN1);
-  myservo[1].attach(SERVOPIN2);
-  digitalWrite(MOTORPIN1, LOW);               // initially stop
-  digitalWrite(MOTORPIN2, LOW);
-  Serial.begin(BAUDRATE);
-  while (!Serial);
-  while (CAN_OK != CAN.begin(CAN_500KBPS))
-  {
-    Serial.println("CAN BUS FAIL!");
-    delay(100);
-  }
-  Serial.println("CAN BUS OK!");
+ pinMode(SWITCH1, INPUT);
+ pinMode(SWITCH2, INPUT); 
+ pinMode(INTERRUPT, INPUT);
+ pinMode(MOTORPIN1, OUTPUT);
+ pinMode(MOTORPIN2, OUTPUT); 
+ pinMode(PWMPIN, OUTPUT);
+ pinMode(SERVOPIN1, OUTPUT);
+ pinMode(SERVOPIN2, OUTPUT); 
+ myservo[0].attach(SERVOPIN1);       
+ myservo[1].attach(SERVOPIN2);            
+ digitalWrite(MOTORPIN1, LOW);               // initially stop
+ digitalWrite(MOTORPIN2, LOW); 
+ attachInterrupt(digitalPinToInterrupt (INTERRUPT), FUNCTION, RISING);
+ Serial.begin(BAUDRATE);
 }
 
-unsigned char idle = 0;
-unsigned char ongoing = 2;
-unsigned char done = 3;
-unsigned char len = 0;
-unsigned char buf[1];
-
-void loop()
+void loop() 
 {
- switch(estate)
- {
-  case IDLE:
+ if(flag==1)                                // if interrupt occurs externally
   {
-    unsigned char len = 0;
-    unsigned char buf[1];
-    if (CAN_MSGAVAIL == CAN.checkReceive())
+    if(CYCLE <=6)                                // count CYCLES
     {
-      estate = START;
-    } 
-    else
-    {
-      CAN.sendMsgBuf(0xAA, 0, 1, &idle);
-      Serial.println("Idle Sent");
-      estate = IDLE;
-    }
-  }
-  break;
-
-  case START:
-  {
-    CAN.readMsgBuf(&len, buf);
-    unsigned long canId = CAN.getCanId();
-    if (buf[0] == 1)
-    {
-      Serial.println("----------Start is received------------");
-      digitalWrite(LED_BUILTIN, HIGH); 
-      estate = RUNNING;
-    }
-    else
-    {
-      Serial.println("----------ERROR COMMAND------------");
-    }
-  }
-  break;
-
-  case RUNNING:
-  {
-    unsigned char len = 0;
-    unsigned char buf[1];
-    if (CAN_MSGAVAIL == CAN.checkReceive())
-    {
-      CAN.readMsgBuf(&len, buf);
-      unsigned long canId = CAN.getCanId();
-      if (buf[0] == 2)
-      {
-       Serial.println("----------Stop is received------------");
-       estate = STOP;
-      }
-    }
-    else
-    {
-        CAN.sendMsgBuf(0xAA, 0, 1, &ongoing);
-        Serial.println("running status sent");
-        if (CYCLE <= 6)                              // count CYCLES
-          {
-           DOWN();                                  // move DOWN
-           DRIVER();                                // set servos to angle according to data
-           Serial.print("SERVO SET TO:");           // serial print status of cycle
-           Serial.println(CYCLE);
-           UP();                                    // move DOWN
-           Serial.println("EXTRACTING DNA");
-           CurrentTime = millis();                   //   delay
-           FutureTime = CurrentTime + DELAY;
-           while (millis() <= FutureTime)
-            {
-//           Serial.println("EXTRACTING DNA");
-              if (CAN_MSGAVAIL == CAN.checkReceive())
-               {
-               CAN.readMsgBuf(&len, buf);
-               unsigned long canId = CAN.getCanId();
-                 if (buf[0] == 2)
-                 {
-                  Serial.println("System INTERRUPTED while EXTRACTION");
-                  estate = STOP;
-                  break;
-                 }
-              }
-            }
-           if (CYCLE == 6)                                   // if last cycle
-           {
-             DOWN();                                          // then go DOWN
-             myservo[0].write(HOME);                          //set servos to home
-             myservo[1].write(HOME);
-             Serial.println("DNA EXTRACTION COMPLETE");       // print completion msg
-             estate = COMPLETE;
-           }
-           CYCLE++;                                             // increment the cycle
-         }
+       DOWN();                                  // move DOWN
+      
+       DRIVER();                                // set servos to angle according to data
+       Serial.print("SERVO SET TO:");           // serial print status of cycle
+       Serial.println(CYCLE);
+       UP();                                    // move DOWN
+      
+       CurrentTime = millis();                   //   delay 
+       FutureTime = CurrentTime + DELAY;         
+       while(millis() <= FutureTime)
+       {
+         Serial.println("EXTRACTING DNA");
+         if(flag!= 1)
+        {
+          Serial.println("INTERRUPT OCCURED");
+          break;
+        }
+       } 
+       if(CYCLE == 6)                                    // if last cycle
+       {   
+          DOWN();                                          // then go DOWN
+        
+          myservo[0].write(HOME);                          //set servos to home
+          myservo[1].write(HOME);                          
+          Serial.println("DNA EXTRACTION COMPLETE");       // print completion msg
+       }
+    CYCLE++;                                             // increment the cycle
     }   
   }
-  break;
-
-  case STOP:
+ else                                                   // if interrupt not occured
   {
-    digitalWrite(LED_BUILTIN, LOW); 
-    Serial.println("RESETTING");             // print status
-    if (digitalRead(SWITCH1) == HIGH)                    // if motor is somewhere in mid move DOW
-    {
+     Serial.println("INTERRUPT NOT OCCURED");             // print status
+     if(digitalRead(SWITCH1) == LOW)                     // if motor is somewhere in mid move DOWN
+    
+     {                                           
       Serial.println("MOVING DOWN");                    // print status of moving DOWN
+    
       digitalWrite(MOTORPIN1, LOW);
       digitalWrite(MOTORPIN2, HIGH);
-      analogWrite(PWMPIN, SPEED);
-    }
-    if(digitalRead(SWITCH1) == LOW)                                             // if motor reaches DOWN
-    {
-      analogWrite(PWMPIN, 0);                           // then stop
+      digitalWrite(PWMPIN, HIGH); 
+     }
+     else                                              // if motor reaches DOWN
+     
+     {
+      digitalWrite(PWMPIN,0);                            // then stop
       myservo[0].write(HOME);                           // set servos at home pos
-      myservo[1].write(HOME);
-      Serial.println("IDLE STATE"); 
-      
-    }
-    estate = IDLE; 
+      myservo[1].write(HOME);  
+      Serial.println("IDLE STATE");                   // print status as idle
+     }
   }
-  break;
-
-  case COMPLETE:
-  {
-    CAN.sendMsgBuf(0xAA, 0, 1, &done);
-    Serial.println("PROCESS COMPLETE");
-    CYCLE = 0;
-    estate = IDLE;
-  }
- } 
+  
 }
 
+void FUNCTION()                                      // if interrupt occurs
+{
+  flag = !flag;                                     // toggle flag state
+}
 
 void DRIVER()                                      // set servos to diff positions based on cycles
 {
-
-  switch (CYCLE)
+  
+  switch(CYCLE)
   {
     case 1:
-      myservo[0].write(30);
-      myservo[1].write(30);
-      break;
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
 
     case 2:
-      myservo[0].write(60);
-      myservo[1].write(60);
-      break;
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
 
     case 3:
-      myservo[0].write(90);
-      myservo[1].write(90);
-      break;
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
 
     case 4:
-      myservo[0].write(120);
-      myservo[1].write(120);
-      break;
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
 
     case 5:
-      myservo[0].write(150);
-      myservo[1].write(150);
-      break;
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
 
     case 6:
-      myservo[0].write(180);
-      myservo[1].write(180);
-      break;
+    myservo[0].write(CYCLE);
+    myservo[1].write(CYCLE);
+    break;
 
     default:
-      myservo[0].write(HOME);
-      myservo[1].write(HOME);
+    myservo[0].write(HOME);
+    myservo[1].write(HOME);
   }
-
+  
 }
 
 void DOWN()                                             // DOWN logic
 {
-  Serial.println("MOVING DOWN");
-  while (digitalRead(SWITCH1) == HIGH)              // while lower limit switch is low move DOWN
-
-  {
-//    Serial.println("MOVING DOWN");
-    digitalWrite(MOTORPIN1, LOW);
-    digitalWrite(MOTORPIN2, HIGH);
-    analogWrite(PWMPIN, SPEED);
-    if (CAN_MSGAVAIL == CAN.checkReceive())
-    {
-      CAN.readMsgBuf(&len, buf);
-      unsigned long canId = CAN.getCanId();
-      if (buf[0] == 2)
+      while(digitalRead(SWITCH1) == LOW)               // while lower limit switch is low move DOWN
+    
       {
-        Serial.println("System INTERRUPTED while going DOWN");
-        estate = STOP;
-        break;
-      }
-    }
-  }
-  analogWrite(PWMPIN, 0);                         // stop the motor
+       Serial.println("MOVING DOWN");
+       digitalWrite(MOTORPIN1, LOW);
+       digitalWrite(MOTORPIN2, HIGH);
+       digitalWrite(PWMPIN,HIGH); 
+       if(flag!= 1)                                   // amid this, if interrupt occurs 
+        {
+        Serial.print("INTERRUPT OCCURED");             // then print and break 
+        break; 
+        }  
+      }     
+      digitalWrite(PWMPIN,0);                          // stop the motor
 }
 
 void UP()
 {
-  Serial.println("MOVING UP");
-  while (digitalRead(SWITCH2) == HIGH)
-  {
-//    Serial.println("MOVING UP");
-    digitalWrite(MOTORPIN1, HIGH);
-    digitalWrite(MOTORPIN2, LOW);
-    analogWrite(PWMPIN, SPEED);
-
-    if (CAN_MSGAVAIL == CAN.checkReceive())
-    {
-      CAN.readMsgBuf(&len, buf);
-      unsigned long canId = CAN.getCanId();
-      if (buf[0] == 2)
+    while(digitalRead(SWITCH2) == LOW)
       {
-        Serial.println("System INTERRUPTED while going UP");
-        estate = STOP;
-        break;
+      Serial.println("MOVING UP");
+      digitalWrite(MOTORPIN1, HIGH);
+      digitalWrite(MOTORPIN2, LOW);
+      digitalWrite(PWMPIN,HIGH);
+
+       if(flag!= 1)
+       {
+        Serial.print("INTERRUPT OCCURED");
+        break; 
+       }  
       }
-    }
-  }
-  analogWrite(PWMPIN, 0);
+      digitalWrite(PWMPIN,0);
 }
